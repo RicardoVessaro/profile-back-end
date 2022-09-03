27f22eb713b08cf379365a666e7ae7dee560f532
ricardovessaro <ricardolvessaro@gmail.com>
2022-09-03 12:06:50 -0300

    chhanginng brackets

--- integrationtest/src/main/java/estudo/s/integrationtest/account/App.java
+++ integrationtest/src/main/java/estudo/s/integrationtest/account/App.java
@@ -1,9 +1,7 @@
 package estudo.s.integrationtest.account;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
